7a0d295cfc086b74f3d91e06ef006baf26eb78b7
ben <1695374+foxdb@users.noreply.github.com>
2023-12-06 20:45:33 +0100

    feat: add link to home from contact

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,11 +1,8 @@
-<script lang="ts" setup>
-
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <h1>Contact</h1>
+  <RouterLink to="/">Home</RouterLink>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
